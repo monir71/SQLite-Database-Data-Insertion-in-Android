46c0743de3d962bff5038614482c17fa96579aef
monir71 <zamanmonir71@yahoo.com>
2025-06-29 09:26:30 +0600

    Insertion

---
+++ app/src/main/java/com/example/sqlitedatainsertion/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.sqlitedatainsertion;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        try {
+            MyDBHelper myDBHelper = new MyDBHelper(this);
+            myDBHelper.addData("Md Moniruzzaman", "01710389323");
+        } catch (RuntimeException e) {
+            throw new RuntimeException(e);
+        }
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
---
+++ app/src/main/java/com/example/sqlitedatainsertion/MyDBHelper.java
@@ -0,0 +1,42 @@
+package com.example.sqlitedatainsertion;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class MyDBHelper extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "contacts";
+    private static final int DATABASE_VERSION = 1;
+    private static final String TABLE_NAME = "contact";
+    private static final String KEY_ID = "id";
+    private static final String KEY_NAME = "name";
+    private static final String KEY_CONTACT = "contact_no";
+
+    public MyDBHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE " + TABLE_NAME + "(" + KEY_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + KEY_NAME + " TEXT NOT NULL, " + KEY_CONTACT + " TEXT NOT NULL)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+
+    public void addData(String name, String phone)
+    {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(KEY_NAME, name);
+        values.put(KEY_CONTACT, phone);
+        db.insert(TABLE_NAME, null, values);
+    }
+}
